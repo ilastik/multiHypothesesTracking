c0489dab199c345eb4990732a0c2edb85d301082
Carsten Haubold <carstenhaubold@googlemail.com>
2015-11-24 08:58:04 +0100

    Add track binary: given model and weights, find the best linking

--- bin/track.cpp
+++ bin/track.cpp
@@ -10,16 +10,16 @@ int main(int argc, char** argv) {
 	namespace po = boost::program_options;
 
 	std::string modelFilename;
-	std::string groundtruthFilename;
-	std::string weightsFilename("weights.json");
+	std::string outputFilename;
+	std::string weightsFilename;
 
 	// Declare the supported options.
 	po::options_description description("Allowed options");
 	description.add_options()
 	    ("help", "produce help message")
 	    ("model,m", po::value<std::string>(&modelFilename), "filename of model stored as Json file")
-	    ("groundtruth,g", po::value<std::string>(&groundtruthFilename), "filename of ground truth stored as Json file")
-	    ("weights,w", po::value<std::string>(&weightsFilename), "filename where the resulting weights will be stored as Json file")
+	    ("weights,w", po::value<std::string>(&weightsFilename), "filename of the weights stored as Json file")
+	    ("output,o", po::value<std::string>(&outputFilename), "filename where the resulting tracking (as links) will be stored as Json file")
 	;
 
 	po::variables_map variableMap;
@@ -31,13 +31,14 @@ int main(int argc, char** argv) {
 	    return 1;
 	}
 
-	if (!variableMap.count("model") || !variableMap.count("groundtruth")) {
-	    std::cout << "Model and Groundtruth filenames have to be specified!" << std::endl;
+	if (!variableMap.count("model") || !variableMap.count("output") || !variableMap.count("weights")) {
+	    std::cout << "Model, Weights and Output filenames have to be specified!" << std::endl;
 	    std::cout << description << std::endl;
 	} else {
 	    Model model;
 		model.readFromJson(modelFilename);
-		std::vector<double> weights = model.learn(groundtruthFilename);
-		saveWeightsToJson(weights, weightsFilename);
+		std::vector<double> weights = readWeightsFromJson(weightsFilename);
+		Solution solution = model.infer(weights);
+		model.saveResultToJson(outputFilename, solution);
 	}
 }
