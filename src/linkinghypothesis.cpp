821623771cfaa6cd75926506cf96f9e583ab47c3
Carsten Haubold <carstenhaubold@googlemail.com>
2015-11-30 08:54:07 +0100

    Split up validity check of JSON entry and be more verbose. + indent using spaces

--- src/linkinghypothesis.cpp
+++ src/linkinghypothesis.cpp
@@ -7,83 +7,85 @@ namespace mht
 {
 
 LinkingHypothesis::LinkingHypothesis():
-	srcId_(-1),
-	destId_(-1)
+    srcId_(-1),
+    destId_(-1)
 {}
 
 LinkingHypothesis::LinkingHypothesis(int srcId, int destId, const helpers::StateFeatureVector& features):
-	srcId_(srcId),
-	destId_(destId),
-	variable_(features)
+    srcId_(srcId),
+    destId_(destId),
+    variable_(features)
 {}
 
 const std::pair<int, int> LinkingHypothesis::readFromJson(const Json::Value& entry)
 {
-	if(!entry.isObject())
-		throw std::runtime_error("Cannot extract LinkingHypothesis from non-object JSON entry");
-	if(!entry.isMember(JsonTypeNames[JsonTypes::SrcId]) || !entry[JsonTypeNames[JsonTypes::SrcId]].isInt() 
-		|| !entry.isMember(JsonTypeNames[JsonTypes::DestId]) || !entry[JsonTypeNames[JsonTypes::DestId]].isInt() 
-		|| !entry.isMember(JsonTypeNames[JsonTypes::Features]) || !entry[JsonTypeNames[JsonTypes::Features]].isArray())
-		throw std::runtime_error("JSON entry for SegmentationHytpohesis is invalid");
+    if(!entry.isObject())
+        throw std::runtime_error("Cannot extract LinkingHypothesis from non-object JSON entry");
+    if(!entry.isMember(JsonTypeNames[JsonTypes::SrcId]) || !entry[JsonTypeNames[JsonTypes::SrcId]].isInt())
+        throw std::runtime_error("JSON entry for LinkingHypothesis is invalid: missing srcId"); 
+    if(!entry.isMember(JsonTypeNames[JsonTypes::DestId]) || !entry[JsonTypeNames[JsonTypes::DestId]].isInt())
+        throw std::runtime_error("JSON entry for LinkingHypothesis is invalid: missing destId");
+    if(!entry.isMember(JsonTypeNames[JsonTypes::Features]) || !entry[JsonTypeNames[JsonTypes::Features]].isArray())
+        throw std::runtime_error("JSON entry for LinkingHypothesis is invalid: missing features");
 
-	srcId_ = entry[JsonTypeNames[JsonTypes::SrcId]].asInt();
-	destId_ = entry[JsonTypeNames[JsonTypes::DestId]].asInt();
+    srcId_ = entry[JsonTypeNames[JsonTypes::SrcId]].asInt();
+    destId_ = entry[JsonTypeNames[JsonTypes::DestId]].asInt();
 
-	// get transition features
-	variable_ = Variable(extractFeatures(entry, JsonTypes::Features));
+    // get transition features
+    variable_ = Variable(extractFeatures(entry, JsonTypes::Features));
 
-	// std::cout << "Found linking hypothesis between " << srcId_ << " and " << destId_ << std::endl;
-	return std::make_pair(srcId_, destId_);
+    // std::cout << "Found linking hypothesis between " << srcId_ << " and " << destId_ << std::endl;
+    return std::make_pair(srcId_, destId_);
 }
 
 void LinkingHypothesis::toDot(std::ostream& stream, const Solution* sol) const
 {
-	stream << "\t" << srcId_ << " -> " << destId_;
+    stream << "\t" << srcId_ << " -> " << destId_;
 
-	size_t value = 0;
-	if(sol != nullptr && variable_.getOpenGMVariableId() >= 0)
-	{
-		value = sol->at(variable_.getOpenGMVariableId());
-		stream << "[ label=\"value=" << value << "\" ";
+    size_t value = 0;
+    if(sol != nullptr && variable_.getOpenGMVariableId() >= 0)
+    {
+        value = sol->at(variable_.getOpenGMVariableId());
+        stream << "[ label=\"value=" << value << "\" ";
 
-		if(value > 0)
-			stream << "color=\"blue\" fontcolor=\"blue\" ]";	
-		else
-			stream << "]";
-	}
+        if(value > 0)
+            stream << "color=\"blue\" fontcolor=\"blue\" ]";    
+        else
+            stream << "]";
+    }
 
-	stream << "; \n" << std::flush;
+    stream << "; \n" << std::flush;
 }
 
 void LinkingHypothesis::registerWithSegmentations(std::map<int, SegmentationHypothesis>& segmentationHypotheses)
 {
-	assert(segmentationHypotheses.find(srcId_) != segmentationHypotheses.end());
-	assert(segmentationHypotheses.find(destId_) != segmentationHypotheses.end());
+    assert(segmentationHypotheses.find(srcId_) != segmentationHypotheses.end());
+    assert(segmentationHypotheses.find(destId_) != segmentationHypotheses.end());
 
-	// std::cout << "Registering outgoing link for " << srcId_ << std::endl;
-	segmentationHypotheses[srcId_].addOutgoingLink(shared_from_this());
-	// std::cout << "Registering incoming link for " << destId_ << std::endl;
-	segmentationHypotheses[destId_].addIncomingLink(shared_from_this());
+    // std::cout << "Registering outgoing link for " << srcId_ << std::endl;
+    segmentationHypotheses[srcId_].addOutgoingLink(shared_from_this());
+    // std::cout << "Registering incoming link for " << destId_ << std::endl;
+    segmentationHypotheses[destId_].addIncomingLink(shared_from_this());
 }
 
 void LinkingHypothesis::addToOpenGMModel(
-	GraphicalModelType& model, 
-	WeightsType& weights, 
-	bool statesShareWeights,
-	const std::vector<size_t>& weightIds)
+    GraphicalModelType& model, 
+    WeightsType& weights, 
+    bool statesShareWeights,
+    const std::vector<size_t>& weightIds)
 {
-	// std::cout << "Adding linking hypothesis between " << srcId_ << " and " << destId_ << " to opengm" << std::endl;
+    // std::cout << "Adding linking hypothesis between " << srcId_ << " and " << destId_ << " to opengm" << std::endl;
 
-	variable_.addToOpenGM(model, statesShareWeights, weights, weightIds);
+    variable_.addToOpenGM(model, statesShareWeights, weights, weightIds);
 }
 
 const Json::Value LinkingHypothesis::toJson(size_t state) const
 {
-	Json::Value val;
-	val[JsonTypeNames[JsonTypes::SrcId]] = Json::Value(srcId_);
-	val[JsonTypeNames[JsonTypes::DestId]] = Json::Value(destId_);
-	val[JsonTypeNames[JsonTypes::Value]] = Json::Value((unsigned int)state);
-	return val;
+    Json::Value val;
+    val[JsonTypeNames[JsonTypes::SrcId]] = Json::Value(srcId_);
+    val[JsonTypeNames[JsonTypes::DestId]] = Json::Value(destId_);
+    val[JsonTypeNames[JsonTypes::Value]] = Json::Value((unsigned int)state);
+    return val;
 }
 
 } // end namespace mht
