4215eb46af43fdc2dfb72eb0f3888888752cf530
Carsten Haubold <carsten.haubold@iwr.uni-heidelberg.de>
2017-10-25 14:26:58 +0200

    fix conda recipe for windows gurobi build

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -2,35 +2,38 @@ mkdir build
 cd build
 
 REM ----------------------------------------------------------------------
-IF NOT DEFINED WITH_CPLEX (SET WITH_CPLEX="")
-IF NOT DEFINED WITH_GUROBI (SET WITH_GUROBI="")
-IF "%WITH_CPLEX%" == "0" (SET WITH_CPLEX="")
-IF "%WITH_GUROBI%" == "0" (SET WITH_GUROBI="")
+IF NOT DEFINED WITH_CPLEX (SET WITH_CPLEX="0")
+IF NOT DEFINED WITH_GUROBI (SET WITH_GUROBI="0")
 
-if "%WITH_CPLEX%" == "" (
-	if "%WITH_GUROBI%" == "" (
+IF "%WITH_CPLEX%" == "0" (
+	if "%WITH_GUROBI%" == "0" (
 		ECHO "Either WITH_CPLEX or WITH_GUROBI must be 1"
 		exit 1
+	)
+	IF NOT DEFINED GUROBI_ROOT_DIR (
+		ECHO "GUROBI_ROOT_DIR must be set for building!"
+		exit 1
+	)
+	IF "%GUROBI_ROOT_DIR%" == "" (
+		ECHO "GUROBI_ROOT_DIR cannot be empty for building!"
+		exit 1
+	) ELSE (
+		ECHO "Using GUROBI_ROOT_DIR=%GUROBI_ROOT_DIR%"
 	)
 	REM if we build with Gurobi, we need to configure the paths.
 	REM MHT chooses gurobi first if that is configured.
 	REM The GUROBI_ROOT_DIR should point to gurobiXYZ\win64
-    SET CPLEX_ARGS=""
-    SET GUROBI_ARGS=""
-    SET GUROBI_ARGS="%GUROBI_ARGS% -DWITH_GUROBI=ON"
-    SET GUROBI_ARGS="%GUROBI_ARGS% -DGUROBI_ROOT_DIR=%GUROBI_ROOT_DIR%"
-    dir "C:\gurobi751\win64\lib\gurobi*.lib" /s/b | findstr gurobi[0-9][0-9].lib > gurobilib.tmp
-    set /p GUROBI_LIB=< gurobilib.tmp
-    ECHO "found gurobi lib %GUROBI_LIB%"
-    SET GUROBI_ARGS="%GUROBI_ARGS% -DGUROBI_LIBRARY=%GUROBI_LIB%"
-    SET GUROBI_ARGS="%GUROBI_ARGS% -DGUROBI_INCLUDE_DIR=%GUROBI_ROOT_DIR%\include"
-    SET GUROBI_ARGS="%GUROBI_ARGS% -DGUROBI_CXX_LIBRARY=%GUROBI_ROOT_DIR%\lib\gurobi_c++mt2015.lib"
-    SET SUFFIX="_with_gurobi"
-)
-else (
+    dir "%GUROBI_ROOT_DIR%\lib\gurobi*.lib" /s/b | findstr gurobi[0-9][0-9].lib > gurobilib.tmp
+    set /p GUROBI_LIB=<gurobilib.tmp
+    ECHO found gurobi lib %GUROBI_LIB%
+    SET GUROBI_ARGS=-DWITH_GUROBI=ON -DGUROBI_ROOT_DIR=%GUROBI_ROOT_DIR% ^
+      -DGUROBI_LIBRARY=%GUROBI_LIB% -DGUROBI_INCLUDE_DIR=%GUROBI_ROOT_DIR%\include ^
+      -DGUROBI_CXX_LIBRARY=%GUROBI_ROOT_DIR%\lib\gurobi_c++md2015.lib
+    SET SUFFIX=_with_gurobi
+) ELSE (
 	REM CPLEX is found automatically if installed. 
 	REM No idea what happens with two CPLEXinstallations, but for now we don't care.
-    SET SUFFIX="_with_cplex"
+    SET SUFFIX=_with_cplex
 )
 
 REM ----------------------------------------------------------------------
